08117559924fe1006c7b21e3efc4bc36ddadb7ca
QassaDevelopers <QassaDevelopers@users.noreply.github.com>
2018-06-07 15:30:35 +0200

    Update routing.js

--- src/plugins/routing.js
+++ src/plugins/routing.js
@@ -34,6 +34,61 @@ function localePathFactory (i18nPath, routerPath) {
   }
 }
 
+function localeRegionPathFactory (i18nPath, routerPath, storePath) {
+  return function localeRegionPath (route, locale) {
+    const store = this[storePath]
+    const router = this[routerPath]
+
+    // Abort if no route or no locale
+    if (!route) return
+    locale = locale || this[i18nPath].locale
+    if (!locale) return
+
+    // If route parameters is a string, use it as the route's name
+    if (typeof route === 'string') {
+      route = { name: route }
+    }
+
+    if (!route.params) route.params = {}
+
+    // Set the region to the preferred region if user is logged in
+    if (store.state.auth.user && store.state.auth.user.preferredRegion) {
+      route.params.region = store.state.auth.user.preferredRegion
+    } else if(!route.params.region) {
+      // TODO hacky, fix this
+      let currentPath = router.history.current.path
+      let region = ''
+
+      let matches = currentPath.match(/([a-zA-Z]{2})-([a-zA-Z]{2})/)
+      if (matches) {
+        // found something like LANGUAGE-REGION (e.g. 'nl-nl', 'en-us', etc)
+        // Only use the last part
+        region = matches[2]
+      }
+      route.params.region = region
+    }
+
+    // Build localized route options
+    // const routesNameSeparator = '<%= options.routesNameSeparator %>'
+    const routesNameSeparator = '___'
+    const name = route.name + routesNameSeparator + locale
+    const localizedRoute = Object.assign({}, route, { name })
+
+    // Resolve localized route
+    const resolved = router.resolve(localizedRoute)
+    let { href } = resolved
+
+    // href = href.replace('fr', route.params.region)
+
+    // Remove baseUrl from href (will be added back by nuxt-link)
+    if (router.options.base) {
+      const regexp = new RegExp(router.options.base)
+      href = href.replace(regexp, '/')
+    }
+
+    return href
+  }
+}
 
 function switchLocalePathFactory (i18nPath) {
   return function switchLocalePath (locale) {
@@ -90,6 +145,7 @@ function getRouteBaseNameFactory (contextRoute) {
 Vue.mixin({
   methods: {
     localePath: localePathFactory('$i18n', '$router'),
+    localeRegionPath: localeRegionPathFactory('$i18n', '$router', '$store'),
     switchLocalePath: switchLocalePathFactory('$i18n'),
     getRouteBaseName: getRouteBaseNameFactory()
   }
@@ -98,6 +154,7 @@ Vue.mixin({
 
 export default ({ app, route }) => {
   app.localePath = localePathFactory('i18n', 'router')
+  app.localeRegionPath = localeRegionPathFactory('i18n', 'router', 'store')
   app.switchLocalePath = switchLocalePathFactory('i18n'),
   app.getRouteBaseName = getRouteBaseNameFactory(route)
 }
